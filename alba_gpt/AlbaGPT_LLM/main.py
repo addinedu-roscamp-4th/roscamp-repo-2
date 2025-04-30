0fb868a9fd1e5957b17d330dc68d70ccf2e36651
JungSeong <jungseonglian@sju.ac.kr>
2025-04-30 17:40:58 +0900

    [BR-95] AlbaGPT_LLM completed

--- alba_gpt/AlbaGPT_LLM/main.py
+++ alba_gpt/AlbaGPT_LLM/main.py
@@ -14,18 +14,25 @@ def main_thread(shared_data) :
     memory = ConversationBufferMemory(memory_key="chat_history", input_key="user_query")
     chat_history = memory.load_memory_variables({})["chat_history"]
 
-    user_query_example_list = ["1번 핑키 안녕!", "오늘 기분이 어때?, 1번 핑키?","3 테이블 청소해, 4번 핑키!", "핑키들, 지금 매장에 도둑이 들었어!", "2번 핑키 4번 테이블이 더럽잖아.", "5번 테이블에 셀러드 하나, 2번 핑키!", "핑키들 5번 테이블 손님 오늘 생일이래", "2번 핑키 카메라 켜봐"]
+    # user_query_example_list = ["1번 핑키 안녕!", "오늘 기분이 어때?, 1번 핑키?","3 테이블 청소해, 4번 핑키!",
+    #                            "핑키들, 지금 매장에 도둑이 들었어!", "2번 핑키 4번 테이블이 더럽잖아.", "5번 테이블에 셀러드 하나, 2번 핑키!",
+    #                            "지금 매장에 불이 났어!", "3번 핑키 오늘 충전 안했지?", "2번 핑키 고생했어.",
+    #                            "3번 핑키 고생했어, 잠시 쉬어.", "1번 핑키 배터리가 없잖아", "2번 핑키 카메라 켜봐",]
 
+    user_query_example_list = ["2번 핑키 카메라 켜봐"]
+    
     for user_query in user_query_example_list :
         discriminated_task = Alba_LLM_templates.validate_alba_task_discriminator(user_query)
+        print(discriminated_task)
 
-        if discriminated_task == "greetings" :
+        if discriminated_task == "GREETINGS" :
             alba_response = Alba_LLM_templates.generate_alba_greetings_response(user_query, chat_history, memory)
-            save_alba_response("greetings", alba_response)
+            save_alba_response("GREETINGS", alba_response)
 
-        elif discriminated_task == "camera on" :
+        elif discriminated_task == "CAMERA" :
+            print("test")
             alba_response = Alba_LLM_templates.generate_alba_camera_on_response(user_query, chat_history, shared_data.latest_frame)
-            save_alba_response("camera on", alba_response)
+            save_alba_response("CAMERA", alba_response)
 
         else :
             for work in alba_work_type_list :
